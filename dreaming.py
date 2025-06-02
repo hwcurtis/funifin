c803eff281886602513b74522642c103f3a5e95f
hwcurtis <hopewcurtis@gmail.com>
2025-06-02 02:33:16 -0400

    getting there

--- dreaming.py
+++ dreaming.py
@@ -1,9 +1,13 @@
 print ("""You have awoken out in the cold darkness, unsure how you came to be here, all of a sudden you
 hear a noise behind you , you see three options, one hiding in the bush on the left, going straight into the unkown, or taking your chances and going right , what will you choose?""")
+print("All of a sudden, you remember this exact situation from somewhere! Then you remember it's from the book you were reading before you fell asleep. Could it be possible that the book transported you? But how? And what is your next move?")
+
 choice = input("type right, left or straight: ")
 if choice == "right":
      print ("you choose to go right and see a small stream , which by chance has a nearby jug")
      print ("you pick up the jug and fill it with water, you are now hydrated")
+
+
 elif choice == "left":
     print("You chose the left path and found a bush. Do you want to pick up a stick or wait?")
     choice2 = input("type stick or wait: ")
